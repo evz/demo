3491b9f7a834410bfd47fc450db15a0981042e9a
Eric <eric@bahai.us>
2011-10-31 03:48:37 -0500

    Another update

--- map_demo/california/urls.py
+++ map_demo/california/urls.py
@@ -5,4 +5,5 @@ from california.views import *
 urlpatterns = patterns('',
     url(r'^counties/$', counties, name='counties'),
     url(r'^places/$', places, name='places'),
+    url(r'^complex-counties/$', complex_counties, name='complex-counties'),
 )
--- map_demo/california/views.py
+++ map_demo/california/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponse
+import json
 from california.models import *
 
 def counties(request):
@@ -9,3 +11,18 @@ def counties(request):
 def places(request):
     places = Place.objects.all()
     return render_to_response('basic-place.html', {'places': places}, context_instance=RequestContext(request))
+
+def complex_counties(request):
+    counties = County.objects.all()
+    bbox = json.dumps(counties.extent())
+    if request.is_ajax():
+        d = {}
+        for county in counties.geojson():
+            geojson = json.loads(county.geojson)
+            properties = {'name': county.name_trans, 'bbox':county.mpoly.extent, 'center':county.mpoly.point_on_surface.coords}
+            geojson['id'] = county.geo_id
+            geojson['properties'] = properties
+            d[county.geo_id] = geojson 
+        return HttpResponse(json.dumps(d), mimetype='application/json')
+    else:
+        return render_to_response('complex-county.html', { 'bbox': bbox }, context_instance=RequestContext(request))
